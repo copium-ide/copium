1d1cd84e0a3e66e2358d6df9308175a1cf1dd140
halu <halufun@proton.me>
2025-12-20 12:03:55 -0800

    update build system

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+APP_NAME="my_game"
+
+# cleanup previous builds
+rm -rf packaged/
+
+# module tree definitions
 input_arg="${1:-debug}"
 result="${input_arg^}"
 
@@ -13,6 +19,15 @@ echo "set(REGULAR_SRCS" >> sources.cmake
 for f in $SRCS; do echo "    \"$f\"" >> sources.cmake; done
 echo ")" >> sources.cmake
 
+# build app
+cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=$result -Dname=$APP_NAME
+cmake --build build --config $result 
 
-cmake -G Ninja -B build -DCMAKE_BUILD_TYPE=$result
-cmake --build build --config $result
+# build shaders
+mkdir -p packaged/assets/shaders/pbr/
+cd source/assets/shaders/pbr/
+glslc -fshader-stage=frag pbr.frag.glsl -o ../../../../packaged/assets/shaders/pbr/pbr.frag.spv
+glslc -fshader-stage=vert pbr.vert.glsl -o ../../../../packaged/assets/shaders/pbr/pbr.vert.spv
+cd ../../../../
+
+cp build/$APP_NAME packaged/$APP_NAME
